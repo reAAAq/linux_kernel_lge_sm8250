cf3e9c2d221898aec18ac087bc75fbd20d906a35
Jianhua Lu <lujianhua000@gmail.com>
2026-07-05 10:37:49 +0800

    power: supply: bq25970: remove unused header

--- drivers/power/supply/bq25970_charger.c
+++ drivers/power/supply/bq25970_charger.c
@@ -27,7 +27,6 @@
 #include <linux/delay.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-#include <linux/of_gpio.h>
 #include <linux/err.h>
 #include <linux/regulator/driver.h>
 #include <linux/regulator/of_regulator.h>
